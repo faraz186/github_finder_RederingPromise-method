8f2e9529b475f0c4ff5b101c720f0a8f7ffa687c
Muhammad Faraz <farazmohammad190@gmail.com>
2024-03-27 19:23:13 +0500

    redering api data

---
+++ github/github.js
@@ -0,0 +1,25 @@
+const getInput = () => {
+  var input = document.getElementById("searchInput").value;
+
+  var correctUsername = input.split(" ").join("");
+  var data = "";
+  axios
+    .get("https://api.github.com/users/" + correctUsername)
+    .then((res) => {
+      data = res.data;
+
+      document.getElementById("result").innerHTML = `
+      <div class="card" style="width: 13rem;">
+  <img src="${data.avatar_url}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${data.name}</h5>
+    <p class="card-text">${data.bio}</p>
+    <a href="#" class="btn btn-primary">${data.html_url}</a>
+  </div>
+</div>
+      `;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
---
+++ redering cards through Promise method/app.js
@@ -0,0 +1,32 @@
+var table = document.getElementById("dataRow");
+
+const fetchData = new Promise((resolve, reject) => {
+  fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+    .then((res) => resolve(res))
+    .catch((err) => reject(err));
+});
+
+fetchData
+  .then((res) => {
+    // console.log(res);
+    sendTohtml(res);
+  })
+  .catch((err) => console.log(err));
+
+function sendTohtml(data) {
+  var card = "";
+  console.log(data);
+  var getData = data.map((e, i) => {
+    card += `<tr>
+    <td>${e.id}</td>
+    <td>${e.title}</td>
+    <td>${e.price}</td>
+    <td>${e.description}</td>
+    <td><img src="${e.image}"/></td>
+
+    </tr>`;
+
+    table.innerHTML = card;
+  });
+}
