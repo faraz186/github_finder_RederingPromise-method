b116d449209a27900d35e098e7675b2ad06d4b9d
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-03-29 20:45:13 +0500

    Update github.js

--- github/github.js
+++ github/github.js
@@ -11,7 +11,7 @@ const getInput = () => {
       document.getElementById("result").innerHTML = `
       <div class="card" style="width: 13rem;">
   <img src="${data.avatar_url}" class="card-img-top" alt="...">
-  <div class="card-body">
+  <div class="card-body">   
     <h5 class="card-title">${data.name}</h5>
     <p class="card-text">${data.bio}</p>
     <a href="#" class="btn btn-primary">${data.html_url}</a>
